1ff637f9e82b7eafc86890c129a37d88f84b8803
karen <karenthrs2@gmail.com>
2025-12-12 09:20:54 -0600

    Se enlaza Login

--- js/layout.js
+++ js/layout.js
@@ -53,7 +53,7 @@ function load_navbar () {
                         <a class="nav-link" href="./objectForm.html">Formulario</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
+                        <a class="nav-link" href="perfil.html">Login</a>
                     </li>
                 </ul>
             </div>
