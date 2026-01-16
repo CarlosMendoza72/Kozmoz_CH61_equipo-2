3cf3a9fa00f5a9fd160f74b3d3ddd189e08f24cb
Adrian Bringas <jraul6711@gmail.com>
2026-01-16 02:40:31 -0600

    Se agrega fetch de JS para la api de login

--- js/main.js
+++ js/main.js
@@ -6,3 +6,63 @@ for (let i = 0; i < accordion.length; i++) {
     this.classList.toggle('active');
   });
 }
+
+// FETCH - USUARIOS ===========================================================================================
+
+const txtEmail = document.getElementById("txtEmail");
+const txtPassword = document.getElementById("txtPassword");
+const btnIngresar = document.getElementById("btnIngresar");
+const URL_MAIN = "http://localhost:8080/api/"; //separado de las githubpages
+//const URL_MAIN = "/api/"; //separado de las githubpages
+
+btnIngresar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        //"name": txtName,               //en nuestra ecommerce Kozmoz
+        "email": txtEmail.value,
+        "password": txtPassword.value
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    }
+
+    fetch (URL_MAIN + "login/", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            console.log("Token", result.accessToken);
+            if (result.accessToken == undefined) {
+                Swal.fire({
+                title: "Login",
+                text: "Nombre de usuario o contraseña incorrectos",
+                icon: "error"
+            });
+            return;
+            }//if result undefined
+            sessionStorage.setItem("token", result.accessToken);
+            Swal.fire({
+                title: "Login",
+                text: "Se ha ingresado satisfactoriamente",
+                icon: "success"
+            });
+            txtEmail.value = "";
+            txtPassword.value = "";
+            //redireccionamiento
+        })/*then*/
+        .catch((error) => {
+            console.error(error);
+            Swal.fire({
+                title: "Login",
+                text: "Se ha producido un error" + error,
+                icon: "error"
+            });
+        });
+});
